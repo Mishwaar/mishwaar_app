516534a71ab045227df01e0c5c19ba69b33d139c
ibr5500 <ibr.s.ahmat@gmail.com>
2024-07-31 20:14:26 +0300

    Edit Rider Model

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -1,3 +1,9 @@
 class Rider < ApplicationRecord
   belongs_to :user
+
+  has_many :rides, class_name: "Rides"
+  has_many :payments, class_name: "Payments"
+
+  validates :payment_method, presence: true
+
 end
