3d5a1cf2863e74594799702df067d74c00387a78
ibr5500 <ibr.s.ahmat@gmail.com>
2024-07-31 20:08:05 +0300

    Edit Driver Model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,9 @@
 class Driver < ApplicationRecord
   belongs_to :user
+
+  has_many :vehicles, class_name: "Vehicles"
+  has_many :rides, class_name: "Rides"
+
+  validates :license_number, presence: true
+  
 end
