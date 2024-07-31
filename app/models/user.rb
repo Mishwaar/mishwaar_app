1618bf4ba538216c88247e2c4a693d9ae64382ae
ibr5500 <ibr.s.ahmat@gmail.com>
2024-07-31 20:03:07 +0300

    Edit User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_one :driver
+  has_one :rider
+
+  enum role: { admin: 0, rider: 1, driver: 2 }
+
+  validates :name, presence :true
+  validates :email, presence: true, uniqueness: true
+  validates :phone, presence: true, uniqueness: true
+
 end
